0fbbcf194a0ad170c885cd9fbd80f6e90bf6c576
Yttehs <shetty.yttehs@outlook.com>
2024-09-24 23:18:02 +0800

    feat: implement print

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,13 +1,18 @@
 #![no_std]
 #![no_main]
 
+#![feature(alloc_error_handler)]
+
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.S"));
 
 mod lang_items;
+mod uart;
 
 #[no_mangle]
 fn rust_main() -> ! {
+    uart::uart_init();
+    uprintln!("Hello, world!");
     loop {}
 }
